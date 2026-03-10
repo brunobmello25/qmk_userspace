3d7086e9fd37f348592e34d817c3f27b628098b4
Bruno Mello <bruno.barros.mello@gmail.com>
2026-03-10 09:58:03 -0300

    add space key on num layer

--- keyboards/crkbd/keymaps/brunobmello25/keymap.c
+++ keyboards/crkbd/keymaps/brunobmello25/keymap.c
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
         KC_LCTL, KC_EQL, KC_MINS, KC_LBRC, KC_RBRC, KC_PLUS,                        KC_NO, KC_LCBR, KC_RCBR,   KC_NO, KC_BSLS, KC_TRNS,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_TRNS, KC_TRNS,  KC_DEL,     KC_TRNS, KC_TRNS, KC_TRNS
+                                          KC_TRNS, KC_TRNS,  KC_SPC,     KC_TRNS, KC_TRNS, KC_TRNS
                                       //`--------------------------'  `--------------------------'
   ),
 
